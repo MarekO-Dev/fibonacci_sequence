295d3bfa71a6e403551232d331d21c62591b9a5a
Marek Olechno <marek.olechno93@gmail.com>
2024-11-19 02:01:46 +0000

    Add
    - Property that returns sequence list
    
    - Property that returns ratios list
    
    - Property that returns ratios dict
    
    - Function that prints out ratios to the terminal in formatted manner

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,13 @@
 from fibonacci_sequence import FibonacciSequence
 
-new_fibonacci_sequence = FibonacciSequence([0], 10)
-sequence_list = new_fibonacci_sequence().return_sequence
+new_fibonacci_sequence = FibonacciSequence([float(1), float(2)], 20)
+sequence = new_fibonacci_sequence(printout=True)
 
-print("The sequence: ", type(sequence_list))
+sequence_list = sequence.return_sequence
+sequence_ratios_list = sequence.return_ratios_list
+sequence_ratios_dict = sequence.return_ratios_dict
+print("SL:", sequence_list)
+print("SRL:", sequence_ratios_list)
+print("SRD:", sequence_ratios_dict)
+
+sequence.printout_sequence_ratio()
--- src/fibonacci_sequence/__init__.py
+++ src/fibonacci_sequence/__init__.py
@@ -1,4 +1,6 @@
 from typing import Any
+import numpy as np
+
 
 class FibonacciSequence:
     """
@@ -11,21 +13,21 @@ class FibonacciSequence:
             # This will get 10 fibonacci sequence values including initial values [0, 1]
             new_fibo_sequence = FibonacciSequence([0, 1], 10)
 
-            # Call it for printed output to the terminal in float format
-            new_fibo_sequence()
+            # Call it with printout=True for printed output to the terminal in float format
+            new_fibo_sequence(printout=True)
 
             Terminal output:
                 '''
-                    1.0000000000
-                    2.0000000000
-                    3.0000000000
-                    5.0000000000
-                    8.0000000000
-                    13.0000000000
-                    21.0000000000
-                    34.0000000000
-                    55.0000000000
-                    89.0000000000
+                    .......0.0000....... <-- initial_value [0]
+                    .......1.0000....... <-- initial_value [1]
+                    .......1.0000.......
+                    .......2.0000.......
+                    .......3.0000.......
+                    .......5.0000.......
+                    .......8.0000.......
+                    ......13.0000.......
+                    ......21.0000.......
+                    ......34.0000.......
                 '''
 
             # Call it and chain .return_sequence property for the actual sequence
@@ -33,36 +35,39 @@ class FibonacciSequence:
             the_sequence = new_fibo_sequence().return_sequence
         ```
     """
-    def __init__(self, initial_items: list = [0, 1], limit: int = 20) -> None:
+    
+    def __init__(self, 
+                 initial_items: list[float] = [float(0), float(1)], 
+                 limit: int = 20) -> None:
         
-        self.initial_items = initial_items
+        self.initial_items: list[float] = initial_items
+        self.sequence_items: list = []
+        self.limit: int = (limit - len(initial_items)) - 1
 
         try:
             if (initial_values_length := len(initial_items)) > 2:
                 raise ValueError("Initial items list must have maximum 2 values to start the sequence with")
+
             elif initial_values_length < 2:
                 raise ValueError("Initial items list must have 2 values to begin the sequence")
+
         except ValueError:
-            print(ValueError)
-            values = []
+
             val1 = input("New first value: ")
             val2 = input("New second value: ")
             
             self.initial_items = []
             self.initial_items.append(float(val1))
             self.initial_items.append(float(val2))
-
+            self.limit = (limit - len(self.initial_items)) - 1
+    
         
-        self.sequence_items: list = []
-        self.limit: int = limit - len(initial_items)
-        
-    def __call__(self) -> Any:
+    def __call__(self, 
+                 printout = False) -> Any:
         
         values_index = 0
-        initial_items = self.initial_items
-
         # Loop through all items in initial items list
-        for item in initial_items:
+        for item in self.initial_items:
             # Add these items to sequence list
             self.sequence_items.append(item)
 
@@ -70,18 +75,80 @@ class FibonacciSequence:
         for _ in self.sequence_items:
             
             # Get the sum of initial numbers and continue moving index to get next ones
-            sum = self.sequence_items[(next_value_index := values_index)]\
-                  + self.sequence_items[(next_value_index + 1)]
+            sum = float(self.sequence_items[(next_value_index := values_index)])\
+                  + float(self.sequence_items[(next_value_index + 1)])
             # Add the sum to the sequence
-            self.sequence_items.append(sum)
-            
-            print(f"{sum:10.3f}")
-            if values_index > self.limit:
+            self.sequence_items.append(float(sum))
+
+            if values_index >= self.limit:
                 break
             values_index += 1
+        
+        if printout:
+
+            for val in self.sequence_items:
+                print(f"{float(val):.^20.4f}")
+
 
         return self
+    
+    def printout_sequence_ratio(self):
+        
+        for k, v in self.return_ratios_dict.items():
+            try:
+                print(f"{k:^30}", " ratio is ", f"{v:^30}")
+            except TypeError:
+                print(f"{k:^30}", " ratio is ", f"{'ZERO':^30}")
+
     @property
     def return_sequence(self) -> list:
         return self.sequence_items
+
+    @property
+    def return_ratios_list(self) -> list:
+        
+        new_sequence: list = []
+        
+        start = 0
+        end = 1
+
+        for _ in self.sequence_items:
+            
+            try:
+                ratio = self.sequence_items[end] / self.sequence_items[start]
+                new_sequence.append(ratio)
+            except ZeroDivisionError:
+                new_sequence.append(None)
+
+            start += 1
+            end += 1
+
+            if end >= len(self.sequence_items):
+                break
+
+        return new_sequence
+    
+    @property
+    def return_ratios_dict(self) -> list:
+        
+        new_sequence: dict = {}
+        
+        start = 0
+        end = 1
+
+        for _ in self.sequence_items:
+            
+            try:
+                ratio = (second := self.sequence_items[end]) / (first := self.sequence_items[start])
+                new_sequence[f"{first}-{second}"] = ratio
+            except ZeroDivisionError:
+                new_sequence[f"{first}-{second}"] = None
+
+            start += 1
+            end += 1
+
+            if end >= len(self.sequence_items):
+                break
+
+        return new_sequence
 
