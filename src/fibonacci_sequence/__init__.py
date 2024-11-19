d9b36cbd375b21b04a230d1a89e09c6da6beeb45
Marek Olechno <marek.olechno93@gmail.com>
2024-11-19 22:05:37 +0000

    Remove some more boilder plate
    Remove useless functions that in reality were useless

--- src/fibonacci_sequence/__init__.py
+++ src/fibonacci_sequence/__init__.py
@@ -27,7 +27,7 @@ class FibonacciSequence:
 
             if values_index >= self.limit:
                 break
-            
+
             values_index += 1
         
         if printout:
@@ -36,50 +36,8 @@ class FibonacciSequence:
                 print(f"{float(val):.^20.4f}")
 
         return self
-    
-    def printout_sequence_ratio(self):
-        
-        for k, v in self.return_ratios_dict.items():
-            try:
-                print(f"{k:^30}", " ratio is ", f"{v:^30}")
-            except TypeError:
-                print(f"{k:^30}", " ratio is ", f"{'ZERO':^30}")
 
-    @property
-    def return_sequence(self) -> list:
-        
-        """
-            ### Return sequence as list
-        """
-
-        return self.initial_sequence
-
-    @property
-    def ratios_list(self) -> list:
-        
-        new_sequence: list = []
-        
-        start = 0
-        end = 1
-
-        for _ in self.initial_sequence:
-            
-            try:
-                ratio = self.initial_sequence[end] / self.initial_sequence[start]
-                new_sequence.append(ratio)
-            except ZeroDivisionError:
-                new_sequence.append(None)
-
-            start += 1
-            end += 1
-
-            if end >= len(self.initial_sequence):
-                break
-
-        return new_sequence
-    
-    @property
-    def ratios_dict(self) -> dict:
+    def ratios_dict(self, *, reversed: bool = False) -> dict:
         
         new_sequence: dict = {}
         
@@ -89,10 +47,15 @@ class FibonacciSequence:
         for _ in self.initial_sequence:
             
             try:
-                ratio = (second := self.initial_sequence[end]) / (first := self.initial_sequence[start])
-                new_sequence[f"({first:^8.0f}, {second:^8.0f})"] = ratio
+                
+                if not reversed:
+
+                    ratio = (second := self.initial_sequence[end]) / (first := self.initial_sequence[start])
+                else:
+                    ratio = (second := self.initial_sequence[start]) / (first := self.initial_sequence[end])
+                new_sequence[f"({first:_^8.0f} {second:_^8.0f})"] = ratio
             except ZeroDivisionError:
-                new_sequence[f"({first:^8.0f}, {second:^8.0f})"] = 0
+                new_sequence[f"({first:_^8.0f} {second:_^8.0f})"] = 0
 
             start += 1
             end += 1
@@ -101,45 +64,19 @@ class FibonacciSequence:
                 break
 
         return new_sequence
-
-    @property
-    def ratios_dict_reversed(self) -> dict:
-        
-        new_sequence: dict = {}
-        
-        start = 0
-        end = 1
-
-        for _ in self.initial_sequence:
-            
-            try:
-                ratio = (first := self.initial_sequence[start]) / (second := self.initial_sequence[end])
-                new_sequence[f"({first:^8.0f} - {second:^8.0f})"] = ratio
-            except ZeroDivisionError:
-                new_sequence[f"{first:^8.0f} {second:^8.0f}"] = 0
-
-            start += 1
-            end += 1
-
-            if end >= len(self.initial_sequence):
-                break
-
-        return new_sequence
-
+    
     @property
     def ratios_df(self):
 
-        series_values = pd.Series(self.ratios_dict.keys())
-        series_ratios = pd.Series(self.ratios_dict.values())
-        df = pd.DataFrame({"Numbers": series_values, "Ratio": series_ratios})
-
-        return df
+        series_values = pd.Series(self.ratios_dict().keys())
+        series_ratios = pd.Series(self.ratios_dict().values())
+        
+        return pd.DataFrame({"Numbers": series_values, "Ratio": series_ratios})
 
     @property
     def ratios_df_reversed(self):
         
-        series_values = pd.Series(self.ratios_dict_reversed.keys())
-        series_ratios = pd.Series(self.ratios_dict_reversed.values())
-        df = pd.DataFrame({"Numbers": series_values, "Ratio": series_ratios})
-
-        return df
+        series_values = pd.Series(self.ratios_dict(reversed = True).keys())
+        series_ratios = pd.Series(self.ratios_dict(reversed = True).values())
+        
+        return pd.DataFrame({"Numbers": series_values, "Ratio": series_ratios})
